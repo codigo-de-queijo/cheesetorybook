58a5939347759b7fa32ddbfea9090b57e295f866
Augusto Meireles <107905267+augustomdn@users.noreply.github.com>
2022-07-13 00:16:01 -0300

    Criação do componente Button (#10)
    * Criação do componente Button
    
    * Dando novas funcionalidades aos botões
    
    * Pão de batata
    
    * Modificações para revisar
    
    * Mais alterações
    
    * Mais mudanças dnv
    
    * fix package lock
    
    Co-authored-by: Guilherme Medeiros <contato@frenetic.com.br>

---
+++ src/components/button/Button.stories.tsx
@@ -0,0 +1,78 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { Button } from './Buttton';
+
+//Título no storybook
+export default {
+    title: 'Botão',
+    component: Button,
+
+    argTypes: {
+        backgroundColor: { control: 'color' },
+        click: { action: 'clicked' },
+    },
+
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+
+Primary.args = {
+    label: 'Primary',
+    click: () => alert('Este é um botão Primário!'),
+    type: 'primary',
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+    label: 'Secondary',
+    click: () => alert('Este é um botão Secundário!'),
+    type: 'secondary',
+
+};
+
+export const Success = Template.bind({});
+Success.args = {
+    label: 'Success',
+    click: () => alert('Este é um botão Success!'),
+    type: 'success',
+};
+
+export const Danger = Template.bind({});
+Danger.args = {
+    label: 'Danger',
+    click: () => alert('Este é um botão Danger!'),
+    type: 'danger',
+};
+
+export const Warning = Template.bind({});
+Warning.args = {
+    label: 'Warning',
+    click: () => alert('Cuidado ao apertar este botão!'),
+    type: 'warning',
+};
+
+export const Info = Template.bind({});
+Info.args = {
+    label: 'Info',
+    click: () => alert('Este botão é Informativo!'),
+    type: 'info',
+};
+
+export const Light = Template.bind({});
+Light.args = {
+    label: 'Light',
+    click: () => alert('Este é um botão Light!'),
+    type: 'light',
+};
+
+export const Dark = Template.bind({});
+Dark.args = {
+    label: 'Dark',
+    click: () => alert('Este é um botão Dark'),
+    type: 'dark',
+};
+
+
+
